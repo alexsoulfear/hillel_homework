d6c7e6e6270f8e4ef867028799d0fdc252bddfa0
alexsoulfear <36886864+alexsoulfear@users.noreply.github.com>
2018-03-13 22:57:14 +0200

    Update homework_task_17.py

--- homework_task_17.py
+++ homework_task_17.py
@@ -1,21 +1,21 @@
 import math
-def solve_quadratic_equation(a, b, c):
-    descr = b ** 2 - 4 * a * c
+def solve_quadratic_equation(a, b, c):# 10, 2, 0
+    descr = (b ** 2) - (4 * a * c)# 4 - 0
     def values_pl (a, b):
-        value_1 = (-b - math.sqrt(descr)) / a * 2
-        value_2 = (-b - math.sqrt(descr)) / a * 2
-        return value_1, value_2
+        value_2 = (-b + math.sqrt(descr)) / (a * 2)
+        value_1 = (-b - math.sqrt(descr)) / (a * 2)
+        return value_2,value_1
     def value_zer (a, b):
-        value = (-b - math.sqrt(descr)) / a * 2
+        value = (-b) / (a * 2)
         return value
     if descr > 0:
         return values_pl(a, b)
     else:
         if descr == 0:
-            return value_zer(a, b)
+            return value_zer(a, b), None
         else:
-            return None
-        
+            return None, None
+
 print(solve_quadratic_equation(2,8,2))
 print(solve_quadratic_equation(1,2,1))
 print(solve_quadratic_equation(2,3,3))
