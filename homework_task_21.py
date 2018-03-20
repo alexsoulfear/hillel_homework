63ea704057ed113efa32e680907225d8a19ebaed
alexsoulfear <36886864+alexsoulfear@users.noreply.github.com>
2018-03-20 21:26:34 +0200

    Update homework_task_21.py

--- homework_task_21.py
+++ homework_task_21.py
@@ -5,7 +5,7 @@ def get_max_digit(number): # returns int
     for _ in range(12):
         num += 1
         if int(numb[num]) > maximum:
-            maximum = (maximum - maximum) + int(numb[num])
+            maximum = int(numb[num])
         else:
             pass
     return maximum
