7c9685f1b92f0b36d17339c33d55efcddc0d421f
Пользователь <pontevx@gmail.com>
2018-03-20 21:29:16 +0200

    homework_19

--- homework_task_19.py
+++ homework_task_19.py
@@ -1,12 +1,13 @@
 import random
 def diff_min_max(num_limit, lower_bound, upper_bound):
-    fst_num = random.randint(lower_bound, upper_bound)
-    scd_num = random.randint(lower_bound, upper_bound)
-    for _ in range (num_limit):
-        if fst_num > scd_num:
-            return fst_num - scd_num
+    max_num = upper_bound
+    min_num = lower_bound
+    for _ in range(num_limit):
+        number = random.randint(lower_bound, upper_bound)
+        if number > min_num:
+            max_num = number
         else:
-            return scd_num - fst_num
-    return int
+            min_num = number
+    return max_num - min_num
 
 print(diff_min_max(9, 0, 15))
