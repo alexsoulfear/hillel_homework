8abd29e81ec3f60a0f60489d8de7b9a1ab943af3
alexsoulfear <36886864+alexsoulfear@users.noreply.github.com>
2018-03-24 21:23:15 +0200

    Update homework_task_24.py

--- homework_task_24.py
+++ homework_task_24.py
@@ -3,16 +3,16 @@ def group_by_surname(list_of_enrollees):
     j_p = []
     q_t = []
     u_z = []
-    for i in range (len(list_of_enrollees)):
+     for i in range (len(list_of_enrollees)):
         name_surname = (list_of_enrollees)[i]
-        surname_first_leter_ord = ord((name_surname[0]))
-        if 65 <= surname_first_leter_ord <= 73:
+        first_letter = name_surname[0]
+        if 'A' <= first_letter <= 'I':
             a_i += [name_surname]
-        if 74 <= surname_first_leter_ord <= 80:
+        if 'J' <= first_letter <= 'P':
             j_p += [name_surname]
-        if 81 <= surname_first_leter_ord <= 84:
+        if 'Q' <= first_letter <= 'T':
             q_t += [name_surname]
-        if 85 <= surname_first_leter_ord <= 90:
+        if 'U' <= first_letter <= 'Z':
             u_z += [name_surname]
     return len(a_i), len(j_p), len(q_t), len(u_z)
  #   return ((name_surname)[0])
