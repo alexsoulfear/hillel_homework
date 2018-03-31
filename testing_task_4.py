cdc90b7bf2ab2582598f1ebf91388f3470ae7917
alexsoulfear <36886864+alexsoulfear@users.noreply.github.com>
2018-03-31 22:32:56 +0300

    Update testing_task_4.py

--- testing_task_4.py
+++ testing_task_4.py
@@ -1,12 +1,9 @@
 def count (number5numeric):
-    total = 0
+    total = 1
     for i in range (len(str(number5numeric))):
         number = int((str(number5numeric))[i])
         if number % 2 == 0:
-            if not total == 0:
-                total *= number
-            if total == 0:
-                total += number
+            total *= number
     return print(total)
 
 count(32323)
