e8372740ec108271dadd0b278cde1dcee2f11fc4
alexsoulfear <36886864+alexsoulfear@users.noreply.github.com>
2018-04-03 23:41:32 +0300

    Update testing_task_8.py

--- testing_task_8.py
+++ testing_task_8.py
@@ -26,6 +26,5 @@ def find_and_change(lst):
     return print(lst)
 #тому, кто придумае более извращённый вариант - лично пожму руку.
 
-find_and_change([1, 1, 1, 1, 1, 1])
 find_and_change([2, 3, 5, 6, 4, 5, 6])
 find_and_change([3, 2, 2, 1, 4, 8, 8, 1, 3, 3, 7, 4, 2])
